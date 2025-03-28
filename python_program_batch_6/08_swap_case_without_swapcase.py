069d96e7426a3cba3bd277add96319851ee3809d
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 17:18:40 +0800

    change some variable name

---
+++ python_program_batch_6/08_swap_case_without_swapcase.py
@@ -0,0 +1,14 @@
+# Ask user for input
+user_input = input("Enter your text: ")
+
+# Swap cases manually
+result = ""
+for char in user_input:
+    if "A" <= char <= "Z":  
+        result += chr(ord(char) + 32)
+    elif "a" <= char <= "z":  
+        result += chr(ord(char) - 32)
+    else:
+        result += char
+
+# Print the result
