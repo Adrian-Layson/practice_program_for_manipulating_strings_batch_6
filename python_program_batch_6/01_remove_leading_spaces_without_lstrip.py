7e108af436379e010c3db3c1c074239ec9fc74f7
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 16:14:44 +0800

    program 01

--- python_program_batch_6/01_remove_leading_spaces_without_lstrip.py
+++ python_program_batch_6/01_remove_leading_spaces_without_lstrip.py
@@ -1,3 +1,12 @@
 # Ask user for input
+user_input = input("Enter your text: ")
+
 # Initiate a program to remove leading spaces
+for i in range(len(user_input)):
+    if user_input[i] != " ":
+        print(user_input[i:])
+        break
+
 # Print the result
+else:
+    print("No text entered. Try again.") 
