1f997ec8eb4ab7db6574b9f0fd5892ca5de8c60a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:35:34 +0800

    program 08

--- python_program_batch_7/08_count_occurrences.py
+++ python_program_batch_7/08_count_occurrences.py
@@ -1,4 +1,16 @@
 # Ask user for input and text to be counted
+user_input = input("Enter your text: ")
+text_counter = input("Enter the text to count: ")
+
 # Initialize
+text = 0
+count = 0
+
 # Loop through every characters
+while text <= len(user_input) - len(text_counter):
+    if user_input[text : text + len(text_counter)] == text_counter:
+        count += 1
+    text += 1
+
 # Print the result
+print(count)
