f0a866f8c6a953e5e22aa0de76065c0cddb0b222
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:39:58 +0800

    program 09

--- python_program_batch_7/09_find_first_occurrence.py
+++ python_program_batch_7/09_find_first_occurrence.py
@@ -1,3 +1,12 @@
 # Ask user for input and text to find
+user_input = input("Enter your text: ")
+target_character = input("Enter the character to find: ")
+
 # Find the first occurence of the character to find
+char = user_input.find(target_character)
+
 # Print the result
+if char != 1:
+    print(char)
+else:
+    print("Character not found")
