3316aac23ed73b74107bd9c778cd83ba787511f8
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:05:44 +0800

    program 04

--- python_program_batch_7/04_check_if_all_lower_case.py
+++ python_program_batch_7/04_check_if_all_lower_case.py
@@ -1,4 +1,15 @@
 # Ask user for input
+user_input = input("Enter your text: ")
+
 # Initialize
+if_lower = True
+
 # Loop through every characters
+for character in user_input:
+    if "A" <= character <= "Z":
+        if_lower = False
+        break
+
+
 # Print
+print(if_lower)
