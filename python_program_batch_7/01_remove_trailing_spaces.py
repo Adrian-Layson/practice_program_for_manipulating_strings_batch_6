c9ee910b280f992e4b6394f7ad166cadbadf6154
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 15:45:10 +0800

    loop added

--- python_program_batch_7/01_remove_trailing_spaces.py
+++ python_program_batch_7/01_remove_trailing_spaces.py
@@ -1,5 +1,12 @@
 # Ask user for input
+user_input = input("Enter your text: ")
+
 # Initialize value from the last character
+start = len(user_input) - 1
+
 # Loop until non-space character is found from right side
+while start >= 0 and user_input[start] == " ":
+    start -= 1
+
 # Remove substring
 # Print the result
