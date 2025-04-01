5ceec564b2ba274a42b7d8f740c4aab9bb80fa6c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:01:48 +0800

    loop initialize

--- python_program_batch_7/03_convert_to_upper_case.py
+++ python_program_batch_7/03_convert_to_upper_case.py
@@ -1,4 +1,14 @@
 # Ask user for input
+user_input = input("Enter your text: ")
+
 # Initialize 
+output = ""
+
 # Loop through every characters
+for character in user_input:
+    if "a" <= character <= "z":
+        output += chr(ord(character)) - 32)
+    else:
+        output += character
+
 # Print
