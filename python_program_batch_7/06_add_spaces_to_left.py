2a83f09bcf48696019799601a39177fdb64ca036
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:22:24 +0800

    checker working

--- python_program_batch_7/06_add_spaces_to_left.py
+++ python_program_batch_7/06_add_spaces_to_left.py
@@ -1,3 +1,11 @@
 # Ask user for input
+user_input = input("Enter your text: ")
+width = int(input("Enter the width: "))
+
 # If text is shorter than width, add spaces
+if len(user_input) < width:
+    result = " " * (width - len(user_input)) + user_input  
+else:
+    result = user_input
+
 # Print the result
