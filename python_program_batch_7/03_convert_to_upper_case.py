8398cc19cd4e1d3077af7507de2c5ac2fceca3fd
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:02:37 +0800

    program 03

--- python_program_batch_7/03_convert_to_upper_case.py
+++ python_program_batch_7/03_convert_to_upper_case.py
@@ -7,8 +7,9 @@ output = ""
 # Loop through every characters
 for character in user_input:
     if "a" <= character <= "z":
-        output += chr(ord(character)) - 32)
+        output += chr(ord(character) - 32)
     else:
         output += character
 
 # Print
+print(output)
