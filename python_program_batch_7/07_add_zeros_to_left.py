6691fb255dd2b66bc1c61dc295c092d696e0ed8b
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:29:22 +0800

    program 07

--- python_program_batch_7/07_add_zeros_to_left.py
+++ python_program_batch_7/07_add_zeros_to_left.py
@@ -1,3 +1,12 @@
 # Ask user for input and desired width
+user_input = input("Enter your text: ")
+width = int(input("Enter the width: "))
+
 # Add zero if text is shorter than width
+if len(user_input) < width:
+    result = "0" * (width - len(user_input)) + user_input
+else:
+    result = user_input
+
 # Print the result
+print(result)
