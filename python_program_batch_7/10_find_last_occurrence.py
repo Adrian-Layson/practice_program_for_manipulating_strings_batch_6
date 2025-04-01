586806a52e579508883aa91d71214b2d64a51c32
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 16:50:02 +0800

    program 10

--- python_program_batch_7/10_find_last_occurrence.py
+++ python_program_batch_7/10_find_last_occurrence.py
@@ -1,3 +1,12 @@
 # Ask user for input and charactert to find
+user_input = input("Enter your text: ")
+target_character = input("Enter the character to find: ")
+
 # Loop through the string from the end
-# Print the result
+for char in range(len(user_input) -1, -1, -1):
+    if user_input[char] == target_character:
+        # Print the result
+        print(char) 
+        break
+else:
+    print("Character not found")
